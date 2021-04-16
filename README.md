12611ded7c81419c854f6bcd7bff5e7c12bfb342
Maxim Eryomenko <moeryomenko@gmail.com>
2021-04-16 16:46:00 +0300

    [tests] rework and extending of testing
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This nginx container with support modules njs and Brotli, also use BoringSSL ins
 
 ```bash
 $ docker run -p 8080:80 ngxjs:v0.0.2
-$ ./test/test.sh
+$ ./tests/test.sh
 
 Test passed!
 ```
---
+++ tests/test.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+[[ -z $@ ]] && host=localhost || host=$@
+
+function basic_case() {
+	curl http://$host:8080 > $DIR/basic_case.html
+	check "basic_case"
+}
+
+function gzip_case() {
+	curl -sH 'Accept-encoding: gzip' http://$host:8080 | gunzip - > $DIR/gzip.html
+	check "gzip"
+}
+
+function brotli_case() {
+	curl -sH 'Accept-encoding: br' http://$host:8080 | brotli --decompress - > $DIR/brotli.html
+	check "brotli"
+}
+
+function check() {
+	if [[ -z `diff $DIR/$1.html $DIR/expected_$1.html` ]]; then
+		rm $DIR/$1.html
+		return
+	fi
+
+	rm $DIR/$1.html
+	echo -e "\n\e[31mTest" $1 "failed!\e[0m\n"
+	exit 1
+}
+
+basic_case
+gzip_case
+brotli_case
+
+echo -e "\n\e[32mTests passed!\e[0m\n"
+exit 0
