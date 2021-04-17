5469bc06b413de1ccc325dcf6e77c76208111f41
Maxim Eryomenko <moeryomenko@gmail.com>
2021-04-17 14:39:14 +0300

    [tests] add image testing
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # ngxjs
 This nginx container with support modules njs and Brotli, also use BoringSSL instead OpenSSL.
 
-## Testing
-
-```bash
-$ docker run -p 8080:80 ngxjs:v0.0.2
-$ ./tests/test.sh
-
-Test passed!
-```
-
 ## Usage
 
 In your Dockerfile just use in `FROM` directive and `COPY` your assets to `/usr/share/nginx/html` folder.
--- tests/test.sh
+++ tests/test.sh
@@ -6,26 +6,41 @@ DIR=$(dirname "$0")
 
 basic_case () {
 	curl http://$host:8080 > $DIR/basic_case.html
-	check "basic_case"
+	check "basic_case.html"
 }
 
 gzip_case () {
 	curl -sH 'Accept-encoding: gzip' http://$host:8080 | gunzip - > $DIR/gzip.html
-	check "gzip"
+	check "gzip.html"
 }
 
 brotli_case () {
 	curl -sH 'Accept-encoding: br' http://$host:8080 | brotli --decompress - > $DIR/brotli.html
-	check "brotli"
+	check "brotli.html"
+}
+
+basic_image_case () {
+	curl http://$host:8080/image.jpg > $DIR/image.jpg
+	check "image.jpg"
+}
+
+gzip_image_case () {
+	curl -sH 'Accept-encoding: gzip' http://$host:8080/image.jpg | gunzip - > $DIR/image.jpg
+	check "image.jpg"
+}
+
+brotli_image_case () {
+	curl -sH 'Accept-encoding: br' http://$host:8080/image.jpg | brotli --decompress - > $DIR/image.jpg
+	check "image.jpg"
 }
 
 check () {
-	if [ -z `diff $DIR/$1.html $DIR/expected_$1.html` ]; then
-		rm $DIR/$1.html
+	if [ -z `diff $DIR/$1 $DIR/expected_$1` ]; then
+		rm $DIR/$1
 		return
 	fi
 
-	rm $DIR/$1.html
+	rm $DIR/$1
 	echo -e "\n\e[31mTest" $1 "failed!\e[0m\n"
 	exit 1
 }
@@ -33,6 +48,9 @@ check () {
 basic_case
 gzip_case
 brotli_case
+basic_image_case
+gzip_image_case
+brotli_image_case
 
 echo -e "\n\e[32mTests passed!\e[0m\n"
 exit 0
