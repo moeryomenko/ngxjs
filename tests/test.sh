9f101281d25478b0fd0fd95b8223afd69f3d1534
Maxim Eryomenko <moeryomenko@gmail.com>
2021-04-22 19:16:58 +0300

    [test] add self-signed cert for ssl connection testing
    cert creation based on https://blog.amosti.net/local-reverse-proxy-with-nginx-mkcert-and-docker-compose/
    
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- tests/test.sh
+++ tests/test.sh
@@ -34,6 +34,36 @@ brotli_image_case () {
 	check "image.jpg"
 }
 
+tls_basic_case () {
+	curl -k https://$host:8083 > $DIR/basic_case.html
+	check "basic_case.html"
+}
+
+tls_gzip_case () {
+	curl -k -sH 'Accept-encoding: gzip' https://$host:8083 | gunzip - > $DIR/gzip.html
+	check "gzip.html"
+}
+
+tls_brotli_case () {
+	curl -k -sH 'Accept-encoding: br' https://$host:8083 | brotli --decompress - > $DIR/brotli.html
+	check "brotli.html"
+}
+
+tls_basic_image_case () {
+	curl -k https://$host:8083/image.jpg > $DIR/image.jpg
+	check "image.jpg"
+}
+
+tls_gzip_image_case () {
+	curl -k -sH 'Accept-encoding: gzip' https://$host:8083/image.jpg | gunzip - > $DIR/image.jpg
+	check "image.jpg"
+}
+
+tls_brotli_image_case () {
+	curl -k -sH 'Accept-encoding: br' https://$host:8083/image.jpg | brotli --decompress - > $DIR/image.jpg
+	check "image.jpg"
+}
+
 check () {
 	if [ -z `diff $DIR/$1 $DIR/expected_$1` ]; then
 		rm $DIR/$1
@@ -51,6 +81,12 @@ brotli_case
 basic_image_case
 gzip_image_case
 brotli_image_case
+tls_basic_case
+tls_gzip_case
+tls_brotli_case
+tls_basic_image_case
+tls_gzip_image_case
+tls_brotli_image_case
 
 echo -e "\n\e[32mTests passed!\e[0m\n"
 exit 0
